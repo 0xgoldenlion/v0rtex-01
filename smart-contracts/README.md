f18e9f26217312f9cc813ca65c16004a7edd9249
goldenFlayingLion <100777093+0xgoldenlion@users.noreply.github.com>
2024-04-27 14:35:59 +0300

    smart-contracts

---
+++ smart-contracts/README.md
@@ -0,0 +1,50 @@
+# comma smart contracts
+
+This folder was generated using [Hardhat](https://github.com/NomicFoundation/hardhat) and contains all the smart contracts used in the comma application.
+
+There are 3 folders inside the contracts folder, one folder per game. Each game has two smart contracts: one contract for game logic (generate boards, verify boards, mint NFT) and another contract to verify the zk proof (this contract was generated using snarkjs).
+
+## Install dependencies
+
+```bash
+yarn install
+```
+
+## Run tests
+
+```bash
+npx hardhat test
+```
+
+When you run tests you will see something like this:
+
+![tests](https://user-images.githubusercontent.com/52170174/166123994-9c68c215-f538-4216-bd50-18d29d4fe4ba.png)
+
+## Deploy on [scroll Testnet](https://sepolia.scrollscan.com/)
+
+Create a `.env` file and add to it:
+
+```text
+PRIVATE_KEY=<yourPrivateKey>
+API_SCROLL = <yourApiKey>
+```
+
+where `yourPrivateKey` is the private key of your wallet.
+
+To deploy on scroll Testnet run:
+
+```bash
+npx hardhat run scripts/deploy.js --network scrollTestnet
+```
+
+## Futoshiki contracts graph
+
+![futoshikiContractGraph](https://user-images.githubusercontent.com/52170174/166124021-7e4ccae1-9872-48a4-8a89-686ff35e1587.svg)
+
+## Skyscrapers contracts graph
+
+![skyscrapersContractGraph](https://user-images.githubusercontent.com/52170174/166124030-6b159ee9-fc06-4f8b-b64f-12427f9324e9.svg)
+
+## Sudoku contracts graph
+
+![sudokuContractGraph](https://user-images.githubusercontent.com/52170174/166124035-d30acdd6-7fea-4016-a872-59179da939ce.svg)
---
+++ smart-contracts/scripts/deploy.js
@@ -0,0 +1,92 @@
+const fs = require("fs");
+const main = async () => {
+  // --------- Sudoku ---------------------
+
+  const contractFactorySudokuVerifier = await hre.ethers.getContractFactory(
+    "SudokuVerifier"
+  );
+  const contractSudokuVerifier = await contractFactorySudokuVerifier.deploy();
+  await contractSudokuVerifier.deployed();
+  console.log(
+    "SudokuVerifier Contract deployed to:",
+    contractSudokuVerifier.address
+  );
+
+  const contractFactorySudoku = await hre.ethers.getContractFactory("Sudoku");
+  const contractSudoku = await contractFactorySudoku.deploy(
+    contractSudokuVerifier.address
+  );
+  await contractSudoku.deployed();
+  console.log("Sudoku Contract deployed to:", contractSudoku.address);
+
+  // --------- Futoshiki ---------------------
+
+  const contractFactoryFutoshikiVerifier = await hre.ethers.getContractFactory(
+    "FutoshikiVerifier"
+  );
+  const contractFutoshikiVerifier =
+    await contractFactoryFutoshikiVerifier.deploy();
+  await contractFutoshikiVerifier.deployed();
+  console.log(
+    "FutoshikiVerifier deployed to:",
+    contractFutoshikiVerifier.address
+  );
+
+  const contractFactoryFutoshiki = await hre.ethers.getContractFactory(
+    "Futoshiki"
+  );
+  const contractFutoshiki = await contractFactoryFutoshiki.deploy(
+    contractFutoshikiVerifier.address
+  );
+  await contractFutoshiki.deployed();
+  console.log("Futoshiki deployed to:", contractFutoshiki.address);
+
+  // --------- Skyscrapers ---------------------
+
+  const contractFactorySkyscrapersVerifier =
+    await hre.ethers.getContractFactory("SkyscrapersVerifier");
+  const contractSkyscrapersVerifier =
+    await contractFactorySkyscrapersVerifier.deploy();
+  await contractSkyscrapersVerifier.deployed();
+  console.log(
+    "SkyscrapersVerifier Contract deployed to:",
+    contractSkyscrapersVerifier.address
+  );
+
+  const contractFactorySkyscrapers = await hre.ethers.getContractFactory(
+    "Skyscrapers"
+  );
+  const contractSkyscrapers = await contractFactorySkyscrapers.deploy(
+    "contractSkyscrapersVerifier.address"
+  );
+  await contractSkyscrapers.deployed();
+  console.log("Skyscrapers Contract deployed to:", contractSkyscrapers.address);
+ 
+  // --------- Print address ---------------------
+ 
+  fs.writeFileSync(
+    "././contracts.js", `
+    export const SudokuVerifier = "${contractSudokuVerifier.address}"
+    export const Sudoku = "${contractSudoku.address}"
+    export const FutoshikiVerifier = "${contractFutoshikiVerifier.address}"
+    export const Futoshiki = "${contractFutoshiki.address}"
+    export const SkyscrapersVerifier = "${contractSkyscrapersVerifier.address}"
+    export const Skyscrapers = "${contractSkyscrapers.address}"
+    `
+  )
+};
+
+
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
+
